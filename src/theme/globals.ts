968187c2caad08d44394ec59322025857a7f435e
Kirill Smorodin <ksmorodin@sbercloud.ru>
2021-04-11 00:54:43 +0300

    ref: globals *

--- src/theme/globals.ts
+++ src/theme/globals.ts
@@ -48,12 +48,19 @@ export const globals = css`
       font-display: swap;
     }
 
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      outline: none;
+      border: none;
+    }
+
     html {
       height: 100%;
     }
 
     body {
-      margin: 0;
       height: 100%;
       background: var(${COLORS_GENERAL.BACKGROUND});
 
