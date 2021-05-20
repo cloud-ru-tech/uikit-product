a66effecd271ea771b1eef533c81f2421d29eaeb
Daniil <dl.khairutdinov@gmail.com>
2021-05-20 15:52:52 +0300

    AICDC-2445 fixed tabs

--- src/components/Tabs/components/Tab/styled.ts
+++ src/components/Tabs/components/Tab/styled.ts
@@ -5,12 +5,13 @@ import { COLORS_TABS } from 'theme/color/vars';
 export const ListItemStyled = styled.li`
   padding: 16px 12px;
   cursor: pointer;
+  font-size: 16px;
   line-height: 24px;
   color: var(${COLORS_TABS.TAB_TEXT_COLOR});
 
   &[data-blue] {
     color: var(${COLORS_TABS.TAB_ACTIVE_COLOR});
-    box-shadow: 0 1px 0 0 var(${COLORS_TABS.TAB_ACTIVE_COLOR});
-    transition: all 500ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    margin-bottom: -1px;
+    border-bottom: 2px solid var(${COLORS_TABS.TAB_ACTIVE_COLOR});
   }
 `;
