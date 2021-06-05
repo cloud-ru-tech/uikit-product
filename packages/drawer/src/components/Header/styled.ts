7f6e0f86b570b47aa916c1540ac3601cbfcab783
Shirochenkov Aleksei <aashirochenkov@sbercloud.ru>
2021-06-05 23:06:48 +0300

    fix: header - semibold | AICDC-2448

--- packages/drawer/src/components/Header/styled.ts
+++ packages/drawer/src/components/Header/styled.ts
@@ -1,7 +1,6 @@
 import { styled } from '@linaria/react';
+import { H3Semibold } from '@sbercloud/uikit-typography';
 
-import { H3 } from '@sbercloud/uikit-typography';
-
-export const HeaderContainerStyled = styled(H3)`
+export const HeaderContainerStyled = styled(H3Semibold)`
   padding: 24px 0;
 `;
