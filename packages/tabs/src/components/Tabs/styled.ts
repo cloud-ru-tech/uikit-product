50bb4c2a18a5d84f8d86a01b610b09ccb1637c11
Трифонов Михаил Сергеевич <mstrifonov@sbercloud.ru>
2021-06-17 08:32:25 +0300

    fix: margin 0 for tabs

--- packages/tabs/src/components/Tabs/styled.ts
+++ packages/tabs/src/components/Tabs/styled.ts
@@ -5,6 +5,7 @@ import { EXPORT_VARS } from '@sbercloud/uikit-theme';
 const { COLORS_TABS } = EXPORT_VARS;
 
 export const GroupStyled = styled.ul`
+  margin: 0;
   display: flex;
   list-style-type: none;
   flex-direction: row;
