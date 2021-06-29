28b40cddc9cd3f619da8368f0319749f000ea039
Daniil <dl.khairutdinov@gmail.com>
2021-06-29 17:06:21 +0300

    fix: AICDC-2903: added required field translation for FormField, fixed help icon

--- packages/form/src/components/FormField/FormField.tsx
+++ packages/form/src/components/FormField/FormField.tsx
@@ -1,31 +1,38 @@
-import { HelpSVG } from '@sbercloud/icons';
+import { QuestionInterfaceSVG } from '@sbercloud/uikit-react-icons';
 import { BasicTooltip } from '@sbercloud/uikit-react-tooltip';
+import { useLanguage } from '@sbercloud/uikit-react-localization';
+import { FORM_FIELD_TRANSLATIONS } from '../../helpers/formFieldTranslations';
 
 import * as S from './styled';
 
 export type FormFieldProps = {
-  label?: string;
   hint?: string;
-  description?: React.ReactNode;
-  required?: boolean;
+  label?: string;
   error?: string;
+  required?: boolean;
+  description?: React.ReactNode;
 };
 
-export const FormField: React.FC<FormFieldProps> = ({ label, hint, description, required, error, children }) => (
-  <S.Wrapper>
-    {label && (
-      <S.Label>
-        {label}
-        {hint && (
-          <BasicTooltip tooltip={hint} classNameTrigger={S.tooltipTriggerClassName}>
-            <HelpSVG size={20} className={S.hintClassName} />
-          </BasicTooltip>
-        )}
-      </S.Label>
-    )}
-    {children}
-    {required && !error && <S.Required>Обязательно для заполнения</S.Required>}
-    {error && <S.Error>{error}</S.Error>}
-    {description && <S.Description>{description}</S.Description>}
-  </S.Wrapper>
-);
+export const FormField: React.FC<FormFieldProps> = ({ label, hint, description, required, error, children }) => {
+  const langCode = useLanguage();
+  const requiredFieldTranslation = FORM_FIELD_TRANSLATIONS.REQUIRED_FIELD[langCode];
+
+  return (
+    <S.Wrapper>
+      {label && (
+        <S.Label>
+          {label}
+          {hint && (
+            <BasicTooltip tooltip={hint} classNameTrigger={S.tooltipTriggerClassName}>
+              <QuestionInterfaceSVG size={20} className={S.hintClassName} />
+            </BasicTooltip>
+          )}
+        </S.Label>
+      )}
+      {children}
+      {required && !error && <S.Required>{requiredFieldTranslation}</S.Required>}
+      {error && <S.Error>{error}</S.Error>}
+      {description && <S.Description>{description}</S.Description>}
+    </S.Wrapper>
+  );
+};
---
+++ packages/form/src/helpers/formFieldTranslations.ts
@@ -0,0 +1,8 @@
+const FORM_FIELD_TRANSLATIONS: { [group: string]: { [languageCode: string]: string } } = {
+  REQUIRED_FIELD: {
+    'ru-RU': 'Обязательно для заполнения',
+    'en-GB': 'Required',
+  },
+};
+
+export { FORM_FIELD_TRANSLATIONS };
