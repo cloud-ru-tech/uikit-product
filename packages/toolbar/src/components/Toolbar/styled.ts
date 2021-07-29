afdd3b562f996d7aca6af65a3e9875f609b1508b
Akhremenko Grigorii <gaahremenko@sbercloud.ru>
2021-07-29 15:49:14 +0300

    fix: add wrapper border

--- packages/toolbar/src/components/Toolbar/styled.ts
+++ packages/toolbar/src/components/Toolbar/styled.ts
@@ -1,11 +1,11 @@
 import { styled } from '@linaria/react';
-
 import { DEPRECATED_EXPORT_VARS } from '@sbercloud/uikit-theme';
 
 const { COLORS_TOOLBAR } = DEPRECATED_EXPORT_VARS;
 
 export const ListToolBarStyled = styled.div`
   background-color: var(${COLORS_TOOLBAR.BACKGROUND});
+  border: 1px solid var(${COLORS_TOOLBAR.INPUT_BORDER});
   width: 100%;
   border-radius: 4px;
   margin: 20px 0;
