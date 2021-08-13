7fdcc42de8c957583eb4371c19b11c4fe8ccd26f
Egor Bibin <evbibin@sbercloud.ru>
2021-08-13 12:00:29 +0300

    feat!: modified divider according to design system (CP-1717)

--- packages/divider/src/components/themes.ts
+++ packages/divider/src/components/themes.ts
@@ -1,7 +1,6 @@
 import { css } from '@linaria/core';
 
 import { EXPORT_VARS, Themes } from '@sbercloud/uikit-theme';
-
 import { Variant } from './constants';
 
 const { GREY, BLACK_ALFA, PURPLE, WHITE_ALFA, GREEN } = EXPORT_VARS;
