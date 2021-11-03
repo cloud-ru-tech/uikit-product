8abd4168bbc93ccdfe58deac31da3754e1f4f039
Akhremenko Grigorii <gaahremenko@sbercloud.ru>
2021-11-03 10:20:43 +0300

    fix: new license key

--- packages/table-private/src/utils/enabledEnterpriseLicenseAgGrid.ts
+++ packages/table-private/src/utils/enabledEnterpriseLicenseAgGrid.ts
@@ -2,6 +2,6 @@ import { LicenseManager } from '@ag-grid-enterprise/core';
 
 export function enabledEnterpriseLicenseAgGrid() {
   const key =
-    'CompanyName=Compuwave GmbH_on_behalf_of_Cloud technology Limited (Ltd.) Russia,LicensedGroup=SberCloud,LicenseType=MultipleApplications,LicensedConcurrentDeveloperCount=3,LicensedProductionInstancesCount=0,AssetReference=AG-011358,ExpiryDate=22_October_2021_[v2]_MTYzNDg1NzIwMDAwMA==2d94c41c7316c6080ab74e7b281a374f';
+    'CompanyName=Syssoft LLC_on_behalf_of_Cloud technology Limited (Ltd.) Russia,LicensedGroup=SberCloud,LicenseType=MultipleApplications,LicensedConcurrentDeveloperCount=6,LicensedProductionInstancesCount=1,AssetReference=AG-019109,ExpiryDate=1_November_2022_[v2]_MTY2NzI2MDgwMDAwMA==b29cb54b103705f43de791c68fb02189';
   LicenseManager.setLicenseKey(key);
 }
