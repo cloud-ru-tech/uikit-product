16a5d806d6d30f3537b8fe322d602a81bba29ecf
Кукарев Михаил <mvkukarev@sbercloud.ru>
2022-01-21 21:08:07 +0300

    fix: correct wrapper width

--- packages/toolbar/src/components/Toolbar/styled.ts
+++ packages/toolbar/src/components/Toolbar/styled.ts
@@ -7,7 +7,6 @@ const { COLORS_TOOLBAR } = DEPRECATED_EXPORT_VARS;
 export const ListToolBarStyled = styled.div`
   background-color: var(${COLORS_TOOLBAR.BACKGROUND});
   border: 1px solid var(${COLORS_TOOLBAR.INPUT_BORDER});
-  width: 100%;
   border-radius: 4px;
   margin: 20px 0;
   height: 40px;
