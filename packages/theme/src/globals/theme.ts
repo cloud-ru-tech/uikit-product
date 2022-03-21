d19fe751ff4151eff3a82b79cf46c983db6cdff2
Белов Алексей <anbelov@sbercloud.ru>
2022-03-21 18:52:20 +0400

    feat: add BACKGROUND_DARK color

--- packages/theme/src/globals/theme.ts
+++ packages/theme/src/globals/theme.ts
@@ -6,6 +6,7 @@ export const GLOBAL_CSS_COLOR = {
   BACKGROUND: '--general-background-color',
   BACKGROUND_ACCENT: '--general-background-accent-color',
   BACKGROUND_SECONDARY: '--general-background-secondary-color',
+  BACKGROUND_DARK: '--general-background-dark-color',
 };
 
 export const globalCSSTheme = `
@@ -13,24 +14,28 @@ export const globalCSSTheme = `
       ${GLOBAL_CSS_COLOR.TEXT}: var(${GREY[800]});
       ${GLOBAL_CSS_COLOR.BACKGROUND}: var(${GREY[50]});
       ${GLOBAL_CSS_COLOR.BACKGROUND_ACCENT}: var(${PURPLE[100]});
+      ${GLOBAL_CSS_COLOR.BACKGROUND_DARK}: var(${BLUE_GREY[90]});
       ${GLOBAL_CSS_COLOR.BACKGROUND_SECONDARY}: var(${GREY[0]});
     }
     body[data-theme='${Themes.PurpleDark}'] {
       ${GLOBAL_CSS_COLOR.TEXT}: var(${GREY[200]});
       ${GLOBAL_CSS_COLOR.BACKGROUND}: var(${GREY[800]});
       ${GLOBAL_CSS_COLOR.BACKGROUND_ACCENT}: var(${PURPLE[100]});
+      ${GLOBAL_CSS_COLOR.BACKGROUND_DARK}: var(${BLUE_GREY[80]});
       ${GLOBAL_CSS_COLOR.BACKGROUND_SECONDARY}: var(${GREY[800]});
     }
     body[data-theme='${Themes.Green}'] {
       ${GLOBAL_CSS_COLOR.TEXT}: var(${GREY[800]});
       ${GLOBAL_CSS_COLOR.BACKGROUND}: var(${GREY[50]});
       ${GLOBAL_CSS_COLOR.BACKGROUND_ACCENT}: var(${BLUE_GREY[80]});
+      ${GLOBAL_CSS_COLOR.BACKGROUND_DARK}: var(${BLUE_GREY[90]});
       ${GLOBAL_CSS_COLOR.BACKGROUND_SECONDARY}: var(${GREY[0]});
     }
     body[data-theme='${Themes.GreenDark}'] {
       ${GLOBAL_CSS_COLOR.TEXT}: var(${GREY[200]});
       ${GLOBAL_CSS_COLOR.BACKGROUND}: var(${GREY[800]});
       ${GLOBAL_CSS_COLOR.BACKGROUND_ACCENT}: var(${GREEN[125]});
+      ${GLOBAL_CSS_COLOR.BACKGROUND_DARK}: var(${BLUE_GREY[80]});
       ${GLOBAL_CSS_COLOR.BACKGROUND_SECONDARY}: var(${GREY[800]});
     }
 `;
