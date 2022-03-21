dd9d9ed7ceb8ef1bbd8c17076c5af62de2443032
Белов Алексей <anbelov@sbercloud.ru>
2022-03-21 13:22:22 +0400

    feat: (AICE-5214) add CheckSmall, Sber, AvatarPlaceholder icons

---
+++ packages/icons/stories/IconsAvatarPlaceholder.tsx
@@ -0,0 +1,40 @@
+import { BADGE } from '@geometricpanda/storybook-addon-badges';
+import { Meta } from '@storybook/react/types-6-0';
+
+import componentChangelog from '../CHANGELOG.md';
+import componentPackage from '../package.json';
+import componentReadme from '../README.md';
+import * as Icons from '../src/components/avatarPlaceholder-icons';
+import { getTemplate } from './helpers/StoryTemplate';
+
+export default {
+  title: 'Components/Icons/Avatar Placeholder',
+} as Meta;
+
+const Template = getTemplate(Icons);
+
+export const avatarPlaceholder = Template.bind({});
+
+avatarPlaceholder.args = {};
+avatarPlaceholder.parameters = {
+  readme: {
+    sidebar: [`Latest version: ${componentPackage.version}`, componentReadme, componentChangelog],
+  },
+  design: {
+    name: 'Figma',
+    type: 'figma',
+    url: 'https://www.figma.com/file/VVqNc0dufYULpLuwIBB84U/TEMP-DESIGN-SYSTEM?node-id=212%3A350',
+  },
+  badges: [BADGE.STABLE],
+};
+avatarPlaceholder.argTypes = {
+  size: {
+    defaultValue: 20,
+    name: '[Stories]: Size of icons',
+    control: {
+      type: 'range',
+      min: 10,
+      max: 200,
+    },
+  },
+};
