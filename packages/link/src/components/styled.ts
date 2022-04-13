aa6259f502c50b27b5b582b472a3607cbde0cd56
Белов Алексей <anbelov@sbercloud.ru>
2022-04-13 16:09:37 +0300

    fix: fixed prefixIcon and suffixIcon colors

--- packages/link/src/components/styled.ts
+++ packages/link/src/components/styled.ts
@@ -1,8 +1,8 @@
 import { styled } from '@linaria/react';
 
 import { ArrowLinkInterfaceSVG } from '@sbercloud/uikit-react-icons';
-import { ANIMATIONS } from '@sbercloud/uikit-utils';
 import { TEXT_1_STYLES, TEXT_2_STYLES, TEXT_3_STYLES } from '@sbercloud/uikit-typography';
+import { ANIMATIONS } from '@sbercloud/uikit-utils';
 
 import { Sizes, Variant } from './constants';
 import { COLORS, GREEN_DARK_THEME, GREEN_THEME, PURPLE_DARK_THEME, PURPLE_THEME } from './themes';
@@ -11,6 +11,10 @@ PURPLE_THEME;
 PURPLE_DARK_THEME;
 GREEN_THEME;
 GREEN_DARK_THEME;
+
+enum LINK_CSS_VARS {
+  prefixIcon = '--link-prefix-icon-color',
+}
 
 export const StyledArrowLinkInterfaceSVG = styled(ArrowLinkInterfaceSVG)`
   margin-left: 4px;
@@ -21,6 +25,7 @@ export const IconWrapper = styled.span`
   justify-content: center;
   align-items: center;
   margin-right: 8px;
+  fill: var(${LINK_CSS_VARS.prefixIcon});
 
   svg {
     width: auto !important;
@@ -42,14 +47,6 @@ export const IconWrapper = styled.span`
     svg {
       height: 24px !important;
     }
-  }
-
-  &[data-variant='${Variant.OnPrimary}'] {
-    fill: var(${COLORS.PRIMARY_PREFIX_ICON});
-  }
-
-  &[data-variant='${Variant.OnDark}'] {
-    fill: var(${COLORS.DARK_PREFIX_ICON});
   }
 `;
 
@@ -80,6 +77,8 @@ export const StyledLink = styled.a<{ showSuffixIcon: boolean }>`
     color: ${({ showSuffixIcon }) => (showSuffixIcon ? `var(${COLORS.PRIMARY_COLOR})` : `var(${COLORS.PRIMARY_FILL})`)};
     fill: ${({ showSuffixIcon }) => (showSuffixIcon ? `var(${COLORS.PRIMARY_FILL})` : 'currentColor')};
 
+    ${LINK_CSS_VARS.prefixIcon}: var(${COLORS.PRIMARY_PREFIX_ICON});
+
     :hover {
       color: var(${COLORS.PRIMARY_FILL_HOVER});
     }
@@ -90,11 +89,16 @@ export const StyledLink = styled.a<{ showSuffixIcon: boolean }>`
 
     &[data-disabled='true'] {
       color: var(${COLORS.PRIMARY_FILL_DISABLED});
+      fill: var(${COLORS.PRIMARY_FILL_DISABLED});
+
+      ${LINK_CSS_VARS.prefixIcon}: var(${COLORS.PRIMARY_PREFIX_ICON_DISABLED});
     }
   }
 
   &[data-variant='${Variant.OnDark}'] {
     color: var(${COLORS.DARK_FILL});
+
+    ${LINK_CSS_VARS.prefixIcon}: var(${COLORS.DARK_PREFIX_ICON});
 
     :hover {
       color: var(${COLORS.DARK_FILL_HOVER});
@@ -106,6 +110,8 @@ export const StyledLink = styled.a<{ showSuffixIcon: boolean }>`
 
     &[data-disabled='true'] {
       color: var(${COLORS.DARK_FILL_DISABLED});
+
+      ${LINK_CSS_VARS.prefixIcon}: var(${COLORS.DARK_PREFIX_ICON_DISABLED});
     }
   }
 `;
--- packages/link/src/components/themes.ts
+++ packages/link/src/components/themes.ts
@@ -11,12 +11,14 @@ export const COLORS = {
   PRIMARY_FILL_ACTIVE: '--color-button-icon-primary-fill-active',
   PRIMARY_FILL_DISABLED: '--color-button-icon-primary-fill-disabled',
   PRIMARY_PREFIX_ICON: '--color-link-primary-prefix-icon',
+  PRIMARY_PREFIX_ICON_DISABLED: '--color-link-primary-prefix-icon-disabled',
 
   DARK_FILL: '--color-link-dark-fill',
   DARK_FILL_HOVER: '--color-link-dark-fill-hover',
   DARK_FILL_ACTIVE: '--color-link-dark-fill-active',
   DARK_FILL_DISABLED: '--color-link-dark-fill-disabled',
   DARK_PREFIX_ICON: '--color-link-dark-prefix-icon',
+  DARK_PREFIX_ICON_DISABLED: '--color-link-dark-prefix-icon-disabled',
 };
 
 export const PURPLE_THEME = css`
@@ -28,12 +30,14 @@ export const PURPLE_THEME = css`
       ${COLORS.PRIMARY_FILL_ACTIVE}: var(${PURPLE[125]});
       ${COLORS.PRIMARY_FILL_DISABLED}: var(${BLACK_ALFA[16]});
       ${COLORS.PRIMARY_PREFIX_ICON}: var(${GREY[200]});
+      ${COLORS.PRIMARY_PREFIX_ICON_DISABLED}: var(${BLACK_ALFA[8]});
 
       ${COLORS.DARK_FILL}: var(${PURPLE[50]});
       ${COLORS.DARK_FILL_HOVER}: var(${PURPLE[25]});
       ${COLORS.DARK_FILL_ACTIVE}: var(${PURPLE[10]});
       ${COLORS.DARK_FILL_DISABLED}: var(${WHITE_ALFA[24]});
       ${COLORS.DARK_PREFIX_ICON}: var(${GREY[450]});
+      ${COLORS.DARK_PREFIX_ICON_DISABLED}: var(${WHITE_ALFA[8]});
     }
   }
 `;
@@ -47,12 +51,14 @@ export const PURPLE_DARK_THEME = css`
       ${COLORS.PRIMARY_FILL_ACTIVE}: var(${PURPLE[10]});
       ${COLORS.PRIMARY_FILL_DISABLED}: var(${WHITE_ALFA[24]});
       ${COLORS.PRIMARY_PREFIX_ICON}: var(${GREY[450]});
+      ${COLORS.PRIMARY_PREFIX_ICON_DISABLED}: var(${WHITE_ALFA[8]});
 
       ${COLORS.DARK_FILL}: var(${PURPLE[50]});
       ${COLORS.DARK_FILL_HOVER}: var(${PURPLE[25]});
       ${COLORS.DARK_FILL_ACTIVE}: var(${PURPLE[10]});
       ${COLORS.DARK_FILL_DISABLED}: var(${WHITE_ALFA[24]});
       ${COLORS.DARK_PREFIX_ICON}: var(${GREY[450]});
+      ${COLORS.DARK_PREFIX_ICON_DISABLED}: var(${WHITE_ALFA[8]});
     }
   }
 `;
@@ -66,12 +72,14 @@ export const GREEN_THEME = css`
       ${COLORS.PRIMARY_FILL_ACTIVE}: var(${GREEN[125]});
       ${COLORS.PRIMARY_FILL_DISABLED}: var(${BLACK_ALFA[16]});
       ${COLORS.PRIMARY_PREFIX_ICON}: var(${GREY[200]});
+      ${COLORS.PRIMARY_PREFIX_ICON_DISABLED}: var(${BLACK_ALFA[8]});
 
       ${COLORS.DARK_FILL}: var(${GREEN[100]});
       ${COLORS.DARK_FILL_HOVER}: var(${GREEN[25]});
       ${COLORS.DARK_FILL_ACTIVE}: var(${GREEN[10]});
       ${COLORS.DARK_FILL_DISABLED}: var(${WHITE_ALFA[24]});
       ${COLORS.DARK_PREFIX_ICON}: var(${GREY[450]});
+      ${COLORS.DARK_PREFIX_ICON_DISABLED}: var(${WHITE_ALFA[8]});
     }
   }
 `;
@@ -85,12 +93,14 @@ export const GREEN_DARK_THEME = css`
       ${COLORS.PRIMARY_FILL_ACTIVE}: var(${GREEN[10]});
       ${COLORS.PRIMARY_FILL_DISABLED}: var(${WHITE_ALFA[24]});
       ${COLORS.PRIMARY_PREFIX_ICON}: var(${GREY[450]});
+      ${COLORS.PRIMARY_PREFIX_ICON_DISABLED}: var(${WHITE_ALFA[8]});
 
       ${COLORS.DARK_FILL}: var(${GREEN[100]});
       ${COLORS.DARK_FILL_HOVER}: var(${GREEN[25]});
       ${COLORS.DARK_FILL_ACTIVE}: var(${GREEN[10]});
       ${COLORS.DARK_FILL_DISABLED}: var(${WHITE_ALFA[24]});
       ${COLORS.DARK_PREFIX_ICON}: var(${GREY[450]});
+      ${COLORS.DARK_PREFIX_ICON_DISABLED}: var(${WHITE_ALFA[8]});
     }
   }
 `;
