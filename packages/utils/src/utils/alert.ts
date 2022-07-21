5b28fe0c91961a0d20f54295daaa699b1f7c4970
Белов Алексей <anbelov@sbercloud.ru>
2022-07-21 18:59:18 +0400

    feat!: remove logMode handling, now it uses '@sbercloud/ft-debug-mode'

--- packages/utils/src/utils/alert.ts
+++ packages/utils/src/utils/alert.ts
@@ -1,29 +1,17 @@
-const STORAGE_KEY = 'UIKIT_PRODUCT_UTILS_LOG_MODE_STORAGE_KEY';
-
-export function isLogModeEnabled() {
-  return STORAGE_KEY in localStorage;
-}
-
-export function enableLogMode() {
-  localStorage.setItem(STORAGE_KEY, '');
-}
-
-export function disableLogMode() {
-  localStorage.removeItem(STORAGE_KEY);
-}
+import { isDebugModeEnabled } from '@sbercloud/ft-debug-mode';
 
 function addMessagePrefix(m: string) {
   return 'uikit-product: ' + m;
 }
 
 export function error(condition: boolean, message: string): void {
-  if (!condition && isLogModeEnabled()) {
+  if (!condition && isDebugModeEnabled()) {
     console.error(addMessagePrefix(message));
   }
 }
 
 export function warning(condition: boolean, message: string): void {
-  if (!condition && isLogModeEnabled()) {
+  if (!condition && isDebugModeEnabled()) {
     console.warn(addMessagePrefix(message));
   }
 }
