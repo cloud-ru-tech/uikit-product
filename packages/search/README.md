75d2e3adf1410f03b3f076e9047c9ab1895c734f
Akhremenko Grigorii <gaahremenko@sbercloud.ru>
2022-08-31 14:47:33 +0300

    feat(FF-838): ref and classname props have been added

--- packages/search/README.md
+++ packages/search/README.md
@@ -3,11 +3,24 @@
 ## Installation
 `npm i @sbercloud/uikit-product-search`
 
-[Changelog](./CHANGELOG.md)
-
 ## Props
 ```typescript
+enum Variant {
+  Transparent = 'transparent',
+  Filled = 'filled',
+  FilledWithStroke = 'filledWithStroke',
+}
+
+enum Size {
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large',
+  ExtraLarge = 'extraLarge',
+}
+
 type SearchProps = {
+  className?: string;
+  ref?: HTMLInputElement;
   value: string;
   onChange(value: string): void;
   disabled?: boolean;
--- packages/search/src/components/Search.tsx
+++ packages/search/src/components/Search.tsx
@@ -1,4 +1,5 @@
-import { useRef } from 'react';
+import mergeRefs from 'merge-refs';
+import { forwardRef, useRef } from 'react';
 
 import { ButtonIcon } from '@sbercloud/uikit-product-button';
 import { CloseInterfaceSVG } from '@sbercloud/uikit-product-icons';
@@ -9,6 +10,7 @@ import { Size, Variant } from './constants';
 import * as S from './styled';
 
 export type SearchProps = WithSupportProps<{
+  className?: string;
   value: string;
   onChange(value: string): void;
   disabled?: boolean;
@@ -17,52 +19,56 @@ export type SearchProps = WithSupportProps<{
   placeholder?: string;
 }>;
 
-export function Search({
-  value,
-  onChange,
-  disabled,
-  size = Size.Medium,
-  variant = Variant.Filled,
-  placeholder,
-  ...rest
-}: SearchProps) {
-  const { languageCode } = useLanguage({ onlyEnabledLanguage: true });
-  const inputRef = useRef<HTMLInputElement>(null);
-  const hasClearButton = value && !disabled;
-  const handleClearSearch = () => {
-    onChange('');
-    inputRef.current?.focus();
-  };
+const ForwardedSearch = forwardRef<HTMLInputElement, SearchProps>(
+  (
+    { className, value, onChange, disabled, size = Size.Medium, variant = Variant.Filled, placeholder, ...rest },
+    ref,
+  ) => {
+    const { languageCode } = useLanguage({ onlyEnabledLanguage: true });
+    const innerRef = useRef<HTMLInputElement>(null);
+    const inputRef = mergeRefs(ref, innerRef);
+    const hasClearButton = value && !disabled;
+    const handleClearSearch = () => {
+      onChange('');
+      innerRef.current?.focus();
+    };
 
-  return (
-    <S.SearchWrapper
-      data-size={size}
-      data-filled={variant === Variant.Filled || variant === Variant.FilledWithStroke || undefined}
-      data-stroke={variant === Variant.FilledWithStroke || undefined}
-      data-disabled={disabled || undefined}
-      {...extractSupportProps(rest)}
-    >
-      <S.Input
-        ref={inputRef}
-        value={value}
-        onChange={onChange}
-        disabled={disabled}
-        placeholder={placeholder || textProvider(languageCode, Texts.Search)}
-        prefix={<S.SearchIcon size={18} data-disabled={disabled || undefined} />}
-        postfix={
-          hasClearButton && (
-            <ButtonIcon
-              icon={<CloseInterfaceSVG size={18} />}
-              onClick={handleClearSearch}
-              tooltip={{ content: textProvider(languageCode, Texts.Clear) }}
-              tabIndex={-1}
-            />
-          )
-        }
-      />
-    </S.SearchWrapper>
-  );
-}
+    return (
+      <S.SearchWrapper
+        className={className}
+        data-size={size}
+        data-filled={variant === Variant.Filled || variant === Variant.FilledWithStroke || undefined}
+        data-stroke={variant === Variant.FilledWithStroke || undefined}
+        data-disabled={disabled || undefined}
+        {...extractSupportProps(rest)}
+      >
+        <S.Input
+          ref={inputRef}
+          value={value}
+          onChange={onChange}
+          disabled={disabled}
+          placeholder={placeholder || textProvider(languageCode, Texts.Search)}
+          prefix={<S.SearchIcon size={18} data-disabled={disabled || undefined} />}
+          postfix={
+            hasClearButton && (
+              <ButtonIcon
+                icon={<CloseInterfaceSVG size={18} />}
+                onClick={handleClearSearch}
+                tooltip={{ content: textProvider(languageCode, Texts.Clear) }}
+                tabIndex={-1}
+              />
+            )
+          }
+        />
+      </S.SearchWrapper>
+    );
+  },
+);
+
+export const Search = ForwardedSearch as typeof ForwardedSearch & {
+  sizes: typeof Size;
+  variants: typeof Variant;
+};
 
 Search.sizes = Size;
 Search.variants = Variant;
