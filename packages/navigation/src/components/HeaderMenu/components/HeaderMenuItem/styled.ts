51ecbe3aace7e45e836f9a1a068e5125fea3f606
Akhremenko Grigorii <gaahremenko@sbercloud.ru>
2022-09-21 09:12:11 +0300

    fix(FF-921): platform selector styles

--- packages/navigation/src/components/HeaderMenu/components/HeaderMenuItem/styled.ts
+++ packages/navigation/src/components/HeaderMenu/components/HeaderMenuItem/styled.ts
@@ -1,5 +1,12 @@
 import { styled } from '@linaria/react';
 
+import { EXPORT_VARS } from '@sbercloud/uikit-product-theme';
+import { TEXT_2_STYLES } from '@sbercloud/uikit-product-typography';
+
 export const Link = styled.a`
+  && svg {
+    fill: var(${EXPORT_VARS.GREY[200]});
+  }
+  ${TEXT_2_STYLES};
   color: unset;
 `;
