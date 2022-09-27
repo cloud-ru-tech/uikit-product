758934380c76f85970cac0420d96272a03335478
Akhremenko Grigorii <gaahremenko@sbercloud.ru>
2022-09-27 17:30:14 +0300

    feat(FF-917): forward ref to components

--- packages/input-slider/src/components/InputRange/InputRange.tsx
+++ packages/input-slider/src/components/InputRange/InputRange.tsx
@@ -1,5 +1,7 @@
 import 'rc-slider/assets/index.css';
 
+import { forwardRef } from 'react';
+
 import { InputDecoratorPrivate } from '@sbercloud/uikit-product-input-decorator-private';
 import { InputPrivate } from '@sbercloud/uikit-product-input-private';
 import { extractSupportProps } from '@sbercloud/uikit-product-utils';
@@ -10,107 +12,115 @@ import { InputRangeProps } from '../types';
 import { useFocus } from '../useFocus';
 import { useMarks } from '../useMarks';
 
-export function InputRange({
-  disabled = false,
-  hint,
-  postfix,
-  label,
-  labelTooltip,
-  marks,
-  max,
-  min,
-  optional,
-  step,
-  value,
-  onChange,
+const ForwarderInputRange = forwardRef<HTMLInputElement, InputRangeProps>(
+  (
+    {
+      disabled = false,
+      hint,
+      postfix,
+      label,
+      labelTooltip,
+      marks,
+      max,
+      min,
+      optional,
+      step,
+      value,
+      onChange,
 
-  ...rest
-}: InputRangeProps) {
-  const { isFocus, onFocus, onBluer } = useFocus();
-  const { privateMarks } = useMarks(min, max, marks);
+      ...rest
+    },
+    ref,
+  ) => {
+    const { isFocus, onFocus, onBluer } = useFocus();
+    const { privateMarks } = useMarks(min, max, marks);
 
-  const changeHandler = (v: number | number[]) => {
-    onChange([v[0], v[1]]);
-  };
+    const changeHandler = (v: number | number[]) => {
+      onChange([v[0], v[1]]);
+    };
 
-  const firstInputHandler = (v: string) => {
-    if (Number(v) > value[1]) {
-      return onChange([value[1], value[1]]);
-    }
+    const firstInputHandler = (v: string) => {
+      if (Number(v) > value[1]) {
+        return onChange([value[1], value[1]]);
+      }
 
-    if (Number(v) < min) {
-      return onChange([min, value[1]]);
-    }
+      if (Number(v) < min) {
+        return onChange([min, value[1]]);
+      }
 
-    onChange([Number(v), value[1]]);
-  };
+      onChange([Number(v), value[1]]);
+    };
 
-  const secondInputHandler = (v: string) => {
-    if (Number(v) < min) {
-      return onChange([min, min]);
-    }
+    const secondInputHandler = (v: string) => {
+      if (Number(v) < min) {
+        return onChange([min, min]);
+      }
 
-    if (Number(v) > max) {
-      return onChange([value[0], max]);
-    }
+      if (Number(v) > max) {
+        return onChange([value[0], max]);
+      }
 
-    if (Number(v) < value[0]) {
-      return onChange([Number(v), Number(v)]);
-    }
+      if (Number(v) < value[0]) {
+        return onChange([Number(v), Number(v)]);
+      }
 
-    onChange([value[0], Number(v)]);
-  };
+      onChange([value[0], Number(v)]);
+    };
 
-  return (
-    <InputDecoratorPrivate
-      hint={hint}
-      label={label}
-      labelTooltip={labelTooltip}
-      optional={optional}
-      {...extractSupportProps(rest)}
-    >
-      <S.InputWrapper data-focus={isFocus} data-disabled={disabled}>
-        <S.ShortBaseInputWrapper width={String(value[0]).length}>
-          <S.ShortBaseInput
-            data-test-id='input-slider__input-min'
-            type={InputPrivate.types.Number}
+    return (
+      <InputDecoratorPrivate
+        hint={hint}
+        label={label}
+        labelTooltip={labelTooltip}
+        optional={optional}
+        {...extractSupportProps(rest)}
+      >
+        <S.InputWrapper data-focus={isFocus} data-disabled={disabled}>
+          <S.ShortBaseInputWrapper width={String(value[0]).length}>
+            <S.ShortBaseInput
+              ref={ref}
+              data-test-id='input-slider__input-min'
+              type={InputPrivate.types.Number}
+              disabled={disabled}
+              value={String(getValueInRange(value[0], min, max))}
+              onChange={firstInputHandler}
+              onFocus={onFocus}
+              onBlur={onBluer}
+            />
+          </S.ShortBaseInputWrapper>
+
+          <S.DelimiterWrapper data-disabled={disabled}>-</S.DelimiterWrapper>
+
+          <S.BaseInput
+            data-test-id={'input-slider__input-max'}
+            value={String(getValueInRange(value[1], min, max))}
+            onChange={secondInputHandler}
             disabled={disabled}
-            value={String(getValueInRange(value[0], min, max))}
-            onChange={firstInputHandler}
             onFocus={onFocus}
             onBlur={onBluer}
           />
-        </S.ShortBaseInputWrapper>
 
-        <S.DelimiterWrapper data-disabled={disabled}>-</S.DelimiterWrapper>
+          {postfix && <S.InputPostfixTextWrapper data-disabled={disabled}>{postfix}</S.InputPostfixTextWrapper>}
+        </S.InputWrapper>
 
-        <S.BaseInput
-          data-test-id={'input-slider__input-max'}
-          value={String(getValueInRange(value[1], min, max))}
-          onChange={secondInputHandler}
+        <S.InputRange
+          range
+          activeDotStyle={S.dotStyle}
           disabled={disabled}
-          onFocus={onFocus}
-          onBlur={onBluer}
+          dotStyle={S.dotStyle}
+          handleStyle={[S.handleStyle(disabled), S.handleStyle(disabled)]}
+          marks={privateMarks}
+          max={max}
+          min={min}
+          railStyle={S.railStyle(disabled)}
+          step={step}
+          trackStyle={[S.trackStyle(disabled)]}
+          value={value}
+          onChange={changeHandler}
         />
+      </InputDecoratorPrivate>
+    );
+  },
+);
 
-        {postfix && <S.InputPostfixTextWrapper data-disabled={disabled}>{postfix}</S.InputPostfixTextWrapper>}
-      </S.InputWrapper>
-
-      <S.InputRange
-        range
-        activeDotStyle={S.dotStyle}
-        disabled={disabled}
-        dotStyle={S.dotStyle}
-        handleStyle={[S.handleStyle(disabled), S.handleStyle(disabled)]}
-        marks={privateMarks}
-        max={max}
-        min={min}
-        railStyle={S.railStyle(disabled)}
-        step={step}
-        trackStyle={[S.trackStyle(disabled)]}
-        value={value}
-        onChange={changeHandler}
-      />
-    </InputDecoratorPrivate>
-  );
-}
+export const InputRange = ForwarderInputRange;
--- packages/input-slider/src/components/InputSlider/InputSlider.tsx
+++ packages/input-slider/src/components/InputSlider/InputSlider.tsx
@@ -1,5 +1,7 @@
 import 'rc-slider/assets/index.css';
 
+import { forwardRef } from 'react';
+
 import { InputDecoratorPrivate } from '@sbercloud/uikit-product-input-decorator-private';
 import { InputPrivate } from '@sbercloud/uikit-product-input-private';
 import { extractSupportProps } from '@sbercloud/uikit-product-utils';
@@ -10,72 +12,80 @@ import { InputSliderProps } from '../types';
 import { useFocus } from '../useFocus';
 import { useMarks } from '../useMarks';
 
-export function InputSlider({
-  disabled = false,
-  hint,
-  postfix,
-  label,
-  labelTooltip,
-  max,
-  min,
-  marks,
-  optional,
-  step,
-  value,
-  onChange,
+const ForwarderInputSlider = forwardRef<HTMLInputElement, InputSliderProps>(
+  (
+    {
+      disabled = false,
+      hint,
+      postfix,
+      label,
+      labelTooltip,
+      max,
+      min,
+      marks,
+      optional,
+      step,
+      value,
+      onChange,
 
-  ...rest
-}: InputSliderProps) {
-  const { isFocus, onFocus, onBluer } = useFocus();
-  const { privateMarks } = useMarks(min, max, marks);
+      ...rest
+    },
+    ref,
+  ) => {
+    const { isFocus, onFocus, onBluer } = useFocus();
+    const { privateMarks } = useMarks(min, max, marks);
 
-  const sliderHandler = (v: number | number[]) => {
-    if (typeof v === 'number') {
-      return onChange(v);
-    }
+    const sliderHandler = (v: number | number[]) => {
+      if (typeof v === 'number') {
+        return onChange(v);
+      }
 
-    onChange(v[0]);
-  };
+      onChange(v[0]);
+    };
 
-  const inputHandler = (v: string) => {
-    onChange(getValueInRange(Number(v), min, max));
-  };
+    const inputHandler = (v: string) => {
+      onChange(getValueInRange(Number(v), min, max));
+    };
 
-  return (
-    <InputDecoratorPrivate
-      hint={hint}
-      label={label}
-      labelTooltip={labelTooltip}
-      optional={optional}
-      {...extractSupportProps(rest)}
-    >
-      <S.InputWrapper data-focus={isFocus} data-disabled={disabled}>
-        <S.BaseInput
-          data-test-id={'input-slider__input'}
-          type={InputPrivate.types.Number}
-          value={String(getValueInRange(value, min, max))}
-          onChange={inputHandler}
+    return (
+      <InputDecoratorPrivate
+        hint={hint}
+        label={label}
+        labelTooltip={labelTooltip}
+        optional={optional}
+        {...extractSupportProps(rest)}
+      >
+        <S.InputWrapper data-focus={isFocus} data-disabled={disabled}>
+          <S.BaseInput
+            ref={ref}
+            data-test-id={'input-slider__input'}
+            type={InputPrivate.types.Number}
+            value={String(getValueInRange(value, min, max))}
+            onChange={inputHandler}
+            disabled={disabled}
+            onFocus={onFocus}
+            onBlur={onBluer}
+          />
+
+          {postfix && <S.InputPostfixTextWrapper data-disabled={disabled}>{postfix}</S.InputPostfixTextWrapper>}
+        </S.InputWrapper>
+
+        <S.InputSlider
           disabled={disabled}
-          onFocus={onFocus}
-          onBlur={onBluer}
+          dotStyle={S.dotStyle}
+          handleStyle={S.handleStyle(disabled)}
+          marks={privateMarks}
+          max={max}
+          min={min}
+          railStyle={S.railStyle(disabled)}
+          step={step}
+          trackStyle={S.trackStyle(disabled)}
+          value={Number(value)}
+          onChange={sliderHandler}
         />
+      </InputDecoratorPrivate>
+    );
+  },
+);
 
-        {postfix && <S.InputPostfixTextWrapper data-disabled={disabled}>{postfix}</S.InputPostfixTextWrapper>}
-      </S.InputWrapper>
-
-      <S.InputSlider
-        disabled={disabled}
-        dotStyle={S.dotStyle}
-        handleStyle={S.handleStyle(disabled)}
-        marks={privateMarks}
-        max={max}
-        min={min}
-        railStyle={S.railStyle(disabled)}
-        step={step}
-        trackStyle={S.trackStyle(disabled)}
-        value={Number(value)}
-        onChange={sliderHandler}
-      />
-    </InputDecoratorPrivate>
-  );
-}
+export const InputSlider = ForwarderInputSlider;
