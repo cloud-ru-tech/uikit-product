e39f56661d663055c6e5cfcb8ef8762a45547020
Белов Алексей <anbelov@sbercloud.ru>
2022-11-28 14:21:53 +0300

    chore: update eslint package version and config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,6 @@
-const baseConfig = require('@sbercloud/ft-all-linters-pack').defaultEslintConfig;
-
 module.exports = {
-  ...baseConfig,
-  plugins: [...baseConfig.plugins, 'testcafe-community'],
-  extends: [...baseConfig.extends, 'plugin:testcafe-community/recommended'],
-  rules: { ...baseConfig.rules, '@typescript-eslint/no-namespace': 'off' },
+  extends: ['@sbercloud/eslint-config/monorepo', 'plugin:testcafe-community/recommended'],
+  plugins: ['testcafe-community'],
+  rules: { '@typescript-eslint/no-namespace': 'off' },
   ignorePatterns: 'packages/icons/src',
-  overrides: [
-    ...baseConfig.overrides,
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: { 'deprecation/deprecation': 'off' },
-    },
-    {
-      files: ['packages/*/stories/*.ts', 'packages/*/stories/*.tsx'],
-      rules: { 'import/no-default-export': 'off' },
-    },
-  ],
 };
