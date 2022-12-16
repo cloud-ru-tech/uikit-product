71ef10de29a8e6c6c8d551ffc15ecec8d6cdd443
Nikita Ershov <niershov@sbercloud.ru>
2022-12-16 12:05:19 +0300

    feat(FF-1426): card panel return

--- packages/cards/src/components/index.ts
+++ packages/cards/src/components/index.ts
@@ -3,6 +3,6 @@ export * from './CardCategory';
 export * from './CardImage';
 export * from './CardQuickAction';
 export * from './CardResult';
-//export * from './CardsPanel';
+export * from './CardsPanel';
 export * from './CardTopic';
 export * from './CardWide';
