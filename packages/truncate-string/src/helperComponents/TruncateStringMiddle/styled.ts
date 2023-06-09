f3748eb0a1d1a0b3af67b3f7ecaeb67c9ae2c33a
Anna Kozlova <apkozlova@cloud.ru>
2023-06-09 15:19:48 +0300

    fix(FF-0000): truncate string width is fit to content

--- packages/truncate-string/src/helperComponents/TruncateStringMiddle/styled.ts
+++ packages/truncate-string/src/helperComponents/TruncateStringMiddle/styled.ts
@@ -2,33 +2,32 @@ import { styled } from '@linaria/react';
 
 export const Container = styled.span`
   position: relative;
-
   overflow: hidden;
-
   white-space: nowrap;
 `;
 
 export const Wrapper = styled.span`
   overflow: hidden;
+  display: block;
+  width: fit-content;
+  max-width: 100%;
 `;
 
 export const Display = styled.span`
   position: absolute;
   left: 0;
-
   display: -webkit-box;
-
   margin: 0;
-
   -webkit-box-orient: vertical;
+  width: fit-content;
+  max-width: 100%;
 `;
 
 export const FullText = styled.span`
   display: -webkit-box;
-
   margin: 0;
-
   opacity: 0;
-
   -webkit-box-orient: vertical;
+  width: fit-content;
+  max-width: 100%;
 `;
