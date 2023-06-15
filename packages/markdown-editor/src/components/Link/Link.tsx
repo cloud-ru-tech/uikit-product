cee1d36ab79bab4742e2fb64d43c43b570a9fb9e
Anna Kozlova <apkozlova@cloud.ru>
2023-06-15 15:32:34 +0300

    chore(FF-2512): fix ts error

--- packages/markdown-editor/src/components/Link/Link.tsx
+++ packages/markdown-editor/src/components/Link/Link.tsx
@@ -1,15 +1,15 @@
+import { AnchorHTMLAttributes, ClassAttributes } from 'react';
 import { ReactMarkdownProps } from 'react-markdown/lib/ast-to-react';
 
 import { Link as UIKitLink } from '@sbercloud/uikit-product-link';
 
 type LinkProps =
-  | (React.ClassAttributes<HTMLAnchorElement> & React.AnchorHTMLAttributes<HTMLAnchorElement> & ReactMarkdownProps)
+  | (ClassAttributes<HTMLAnchorElement> & AnchorHTMLAttributes<HTMLAnchorElement> & ReactMarkdownProps)
   | keyof JSX.IntrinsicElements
   | undefined;
 
 export function Link(props: LinkProps) {
   if (typeof props === 'object' && 'children' in props) {
-    // @ts-expect-error в некоторых сценариях в props.children может быть разметка
     return <UIKitLink text={props.children} href={props.href || ''} />;
   }
   return <div />;
