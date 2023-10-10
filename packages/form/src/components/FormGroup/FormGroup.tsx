bfeb504140ba9b3b101c07f4f9bb2dfc63714970
Белов Алексей <anbelov@cloud.ru>
2023-10-10 18:50:17 +0400

    feat(FF-3462): add supportProps to FormGroup

--- packages/form/src/components/FormGroup/FormGroup.tsx
+++ packages/form/src/components/FormGroup/FormGroup.tsx
@@ -1,20 +1,23 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, ReactNode } from 'react';
 
 import { QuestionSmallOutlineInterfaceSVG } from '@sbercloud/uikit-product-icons';
 import { Tooltip, TooltipProps } from '@sbercloud/uikit-product-tooltip';
+import { extractSupportProps, WithSupportProps } from '@sbercloud/uikit-product-utils';
 
 import { Content, Header, hintClassName, Number, Title, tooltipTriggerClassName, Wrapper } from './styled';
 
-export type FormGroupProps = PropsWithChildren<{
-  number?: number;
-  className?: string;
-  title?: React.ReactNode;
-  hint?: Omit<TooltipProps, 'children' | 'type'>;
-}>;
+export type FormGroupProps = WithSupportProps<
+  PropsWithChildren<{
+    number?: number;
+    className?: string;
+    title?: ReactNode;
+    hint?: Omit<TooltipProps, 'children' | 'type'>;
+  }>
+>;
 
-export function FormGroup({ className, number, title, hint, children }: FormGroupProps) {
+export function FormGroup({ className, number, title, hint, children, ...rest }: FormGroupProps) {
   return (
-    <Wrapper className={className}>
+    <Wrapper className={className} {...extractSupportProps(rest)}>
       {title && (
         <Header>
           {typeof number === 'number' && <Number>{number}</Number>}
