7db2337ffa04ed40e9e0bbc6bbcf10bb31aef3fd
Anna Kozlova <apkozlova@cloud.ru>
2024-02-02 12:26:48 +0300

    chore(FF-4183): fix story

--- packages/carousel/stories/Carousel.tsx
+++ packages/carousel/stories/Carousel.tsx
@@ -84,9 +84,6 @@ const cardsMock = [
         title={'Заголовок'}
         description={'Описание для карточки'}
         actionLabel={'Открыть'}
-        emblem={{
-          icon: UserInterfaceSVG,
-        }}
         image={{
           src: 'https://images.unsplash.com/photo-1655720031554-a929595ffad7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80',
           alt: '',
