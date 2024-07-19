18bd93add51aee9e1f8aa7dc32802bc10de6d344
Anna Kozlova <apkozlova@cloud.ru>
2024-07-19 11:14:55 +0300

    fix(FF-5224)!: delete colored service illustrations

---
+++ packages/icons/__tests__/service-icons.ts
@@ -0,0 +1,14 @@
+import { fixture, Selector, test } from 'testcafe';
+
+import { dataTestIdSelector, getTestcafeUrl } from '../../../testcafe/utils';
+import { getIconsDataTestIds } from '../utils/getIconsDataTestIds';
+
+const serviceIcons = getIconsDataTestIds('svgs/service-icons');
+
+fixture('Icons: Service Icons').page(getTestcafeUrl({ name: 'service', group: 'icons', category: 'snack-uikit' }));
+
+test('Rendered', async t => {
+  for (const icon of serviceIcons) {
+    await t.expect(Selector(dataTestIdSelector(icon)).exists).ok(`${icon}`);
+  }
+});
--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -1,13 +1,12 @@
 export { Sprite } from '@snack-uikit/icons';
 
 export * from './components/flag-icons';
-export * from './components/services-icons';
+export * from './components/services-icons-old';
 export * from './components/extension-icons';
 export * from './components/logo-icons';
 export * from './components/interface-icons-old';
 export * from './components/interface-icons-product';
 export * from './components/interface-icons-system';
-export * from './components/service-illustrations-themed-icons';
-export * from './components/service-illustrations-icons';
+export * from './components/service-icons';
 
 export { default as SpriteSystemSVG } from './sprite/svg/sprite-system.symbol.svg';
