4a2ae517ccf4920ac569bd2a0a716785f49309e8
Anna Kozlova <apkozlova@cloud.ru>
2024-08-02 14:52:46 +0300

    feat(PDS-559): export list helpres

--- packages/mobile-dropdown/src/index.ts
+++ packages/mobile-dropdown/src/index.ts
@@ -1,1 +1,21 @@
 export * from './components';
+export {
+  isBaseItemProps,
+  isAccordionItemProps,
+  isGroupItemProps,
+  isGroupSelectItem,
+  isNextListItemProps,
+} from '@snack-uikit/list';
+
+export type {
+  BaseItemProps,
+  DroplistProps,
+  GroupItemProps,
+  GroupSelectItemProps,
+  ItemContentProps,
+  ItemId,
+  NextListItemProps,
+  SelectionMultipleState,
+  SelectionSingleState,
+  SelectionSingleValueType,
+} from '@snack-uikit/list';
