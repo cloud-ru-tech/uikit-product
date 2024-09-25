e23f22af39a954d6607153b9d4c438f442092421
Данил Панов <dapanov@cloud.ru>
2024-09-25 15:31:36 +0300

    fix(ONB-2187): fix contexts for adaptive stepper

--- packages/mobile-stepper/src/components/AdaptiveStepper/AdaptiveStepper.tsx
+++ packages/mobile-stepper/src/components/AdaptiveStepper/AdaptiveStepper.tsx
@@ -1,11 +1,19 @@
 import { WithLayoutType } from '@sbercloud/uikit-product-utils';
-import { Stepper, StepperProps } from '@snack-uikit/stepper';
+import { Stepper, StepperProps, useStepperApi } from '@snack-uikit/stepper';
 
+import { useMobileStepperApi } from '../../MobileStepperContext';
 import { MobileStepper } from '../MobileStepper';
 
 export function AdaptiveStepper({ layoutType, ...props }: WithLayoutType<StepperProps>) {
   const isMobile = layoutType === 'mobile';
   return isMobile ? <MobileStepper {...props} /> : <Stepper {...props} />;
+}
+
+export function useAdaptiveStepperApi({ layoutType }: WithLayoutType) {
+  const isMobile = layoutType === 'mobile';
+  const mobileSteperApi = useMobileStepperApi();
+  const steperApi = useStepperApi();
+  return isMobile ? mobileSteperApi : steperApi;
 }
 
 export type { StepperProps };
