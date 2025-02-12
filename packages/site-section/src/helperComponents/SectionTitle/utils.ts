f900beb8b806c7afa23410d06457b3589e9487ac
Belyavskiy Ilya <iabelyavskiy@cloud.ru>
2025-02-12 09:56:10 +0300

    fix(PDS-1617): changed typography props for site section title mobile

--- packages/site-section/src/helperComponents/SectionTitle/utils.ts
+++ packages/site-section/src/helperComponents/SectionTitle/utils.ts
@@ -51,8 +51,8 @@ export const getTitleTypographyProps = ({
         case 'm':
         default:
           return {
-            purpose: 'title',
-            size: 'l',
+            purpose: 'headline',
+            size: 's',
           };
       }
 
