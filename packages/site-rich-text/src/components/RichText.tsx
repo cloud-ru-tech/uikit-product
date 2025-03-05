6f0c2c7ced1aa63e599d1325f8cb03c0f2ba2a61
Anna Kozlova <apkozlova@cloud.ru>
2025-03-05 14:44:17 +0300

    feat(PDS-1535): add data-test-id

--- packages/site-rich-text/src/components/RichText.tsx
+++ packages/site-rich-text/src/components/RichText.tsx
@@ -1,12 +1,20 @@
 import cn from 'classnames';
 
+import { extractSupportProps, WithSupportProps } from '@sbercloud/uikit-product-utils';
+
 import styles from './styles.module.scss';
 
-export type RichTextProps = {
+export type RichTextProps = WithSupportProps<{
   richText?: string;
   className?: string;
-};
+}>;
 
-export function RichText({ richText = '', className }: RichTextProps) {
-  return <div className={cn(styles.richText, className)} dangerouslySetInnerHTML={{ __html: richText }} />;
+export function RichText({ richText = '', className, ...rest }: RichTextProps) {
+  return (
+    <div
+      className={cn(styles.richText, className)}
+      dangerouslySetInnerHTML={{ __html: richText }}
+      {...extractSupportProps(rest)}
+    />
+  );
 }
