b54260b03e9a69f28ca36ac02c53f7c1f4e9c743
Белов Алексей <anbelov@cloud.ru>
2025-03-21 16:13:28 +0400

    fix(PDS-1946): use RichText in PromoList

--- packages/site-section/src/helperComponents/PromoList/PromoList.tsx
+++ packages/site-section/src/helperComponents/PromoList/PromoList.tsx
@@ -1,4 +1,5 @@
 import { DecorCheckedSVG } from '@sbercloud/uikit-product-icons';
+import { RichText } from '@sbercloud/uikit-product-site-rich-text';
 import { WithLayoutType } from '@sbercloud/uikit-product-utils';
 
 import styles from './styles.module.scss';
@@ -9,14 +10,14 @@ export function PromoList({ title, items, layoutType }: WithLayoutType<PromoList
   return (
     <div className={styles.promoList} data-layout-type={layoutType}>
       <div className={styles.title} data-layout-type={layoutType} data-test-id='promo-list__card_title'>
-        {title}
+        <RichText richText={title} />
       </div>
 
       <ul className={styles.list}>
         {items.map(item => (
           <li className={styles.listItem} key={item}>
             <DecorCheckedSVG className={styles.listItemIcon} />
-            {item}
+            <RichText richText={item} />
           </li>
         ))}
       </ul>
