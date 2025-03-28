645cb2d07b4b3bae420af3e848f007865d1acd51
Белов Алексей <anbelov@cloud.ru>
2025-03-28 15:39:27 +0400

    feat(PDS-2016): allow rich-text to pass ReactNode

--- packages/site-rich-text/src/components/RichText.tsx
+++ packages/site-rich-text/src/components/RichText.tsx
@@ -1,19 +1,22 @@
 import cn from 'classnames';
+import { ReactNode } from 'react';
 
 import { extractSupportProps, WithSupportProps } from '@sbercloud/uikit-product-utils';
 
 import styles from './styles.module.scss';
 
 export type RichTextProps = WithSupportProps<{
-  richText?: string;
+  richText?: string | ReactNode;
   className?: string;
 }>;
 
 export function RichText({ richText = '', className, ...rest }: RichTextProps) {
+  const isStringContent = typeof richText === 'string';
+
   return (
     <div
       className={cn(styles.richText, className)}
-      dangerouslySetInnerHTML={{ __html: richText }}
+      {...(isStringContent ? { dangerouslySetInnerHTML: { __html: richText } } : { children: richText })}
       {...extractSupportProps(rest)}
     />
   );
