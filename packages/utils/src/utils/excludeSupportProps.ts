307581a22bc5f3b4829ce947f203c055bb1873fd
Белов Алексей <anbelov@cloud.ru>
2025-08-04 21:40:54 +0400

    fix(FF-7076): missing constant usage

--- packages/utils/src/utils/excludeSupportProps.ts
+++ packages/utils/src/utils/excludeSupportProps.ts
@@ -1,6 +1,6 @@
-import { DATA_TEST_AND_ARIA_REGEXP } from './private/constants';
+import { DATA_AND_ARIA_REGEXP } from './private/constants';
 import { excludeProps } from './private/excludeProps';
 
 export function excludeSupportProps(props: Record<string, unknown>) {
-  return excludeProps(props, DATA_TEST_AND_ARIA_REGEXP);
+  return excludeProps(props, DATA_AND_ARIA_REGEXP);
 }
