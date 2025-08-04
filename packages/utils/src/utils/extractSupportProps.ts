2c9e8c446c05b6dcc933e1e3987608ee815b5175
Белов Алексей <anbelov@cloud.ru>
2025-08-04 11:45:11 +0400

    feat(FF-7076)!: extract all data and aria attributes with single helper, remove unused helpers

--- packages/utils/src/utils/extractSupportProps.ts
+++ packages/utils/src/utils/extractSupportProps.ts
@@ -1,6 +1,6 @@
-import { DATA_TEST_AND_ARIA_REGEXP } from './private/constants';
+import { DATA_AND_ARIA_REGEXP } from './private/constants';
 import { extractProps } from './private/extractProps';
 
 export function extractSupportProps(props: Record<string, unknown>) {
-  return extractProps(props, DATA_TEST_AND_ARIA_REGEXP);
+  return extractProps(props, DATA_AND_ARIA_REGEXP);
 }
