7886e7c6fd5fa5cc2cd4c3129e8fc3392785eba3
Alexandr Petrov <alapetrov@cloud.ru>
2025-10-14 15:25:24 +0400

    feat(GIGAID-449): add export

--- packages/fields-predefined/src/components/FieldPhone/index.ts
+++ packages/fields-predefined/src/components/FieldPhone/index.ts
@@ -4,4 +4,4 @@ export type { FieldPhoneOptionsProps, CountrySettings } from './types';
 
 export * from './countries';
 export * from './hooks';
-export { formatPhoneNumber } from './utils';
+export { formatPhoneNumber, detectCountryByPhone } from './utils';
