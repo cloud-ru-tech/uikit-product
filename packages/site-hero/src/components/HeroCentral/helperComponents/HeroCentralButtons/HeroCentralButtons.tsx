f554b7cf869fff88eee90b52a4f652a04b04afd0
daparfirov <daparfirov@cloud.ru>
2026-07-21 12:16:42 +0400

    feat(SITE-11480): update heroCentral

--- packages/site-hero/src/components/HeroCentral/helperComponents/HeroCentralButtons/HeroCentralButtons.tsx
+++ packages/site-hero/src/components/HeroCentral/helperComponents/HeroCentralButtons/HeroCentralButtons.tsx
@@ -1,16 +1,21 @@
 import { WithLayoutType } from '@cloud-ru/uikit-product-utils';
-import { ButtonFilled, ButtonOutline } from '@snack-uikit/button';
+import { ButtonFilled, ButtonFilledProps, ButtonOutline } from '@snack-uikit/button';
 
 import styles from './styles.module.scss';
 
 export type ButtonStyle = 'filled' | 'outline';
+
+type ButtonProps = Pick<
+  ButtonFilledProps,
+  'label' | 'href' | 'className' | 'fullWidth' | 'size' | 'target' | 'onClick'
+>;
 
 export type HeroCentralButton = {
   text: string;
   uniqueId: string;
   link?: string;
   style?: ButtonStyle;
-  onClick?: () => void;
+  onClick?: ButtonProps['onClick'];
 };
 
 type HeroCentralButtonsProps = WithLayoutType<{
@@ -23,18 +28,18 @@ export function HeroCentralButtons({ buttons, layoutType }: HeroCentralButtonsPr
   return (
     <div className={styles.buttonsContainer} data-layout-type={layoutType}>
       {buttons.map(button => {
-        const buttonProps = {
-          key: button.uniqueId,
+        const buttonProps: ButtonProps = {
           label: button.text,
           href: button.link,
           className: styles.button,
           fullWidth: ['mobile', 'tablet'].includes(layoutType),
-          size: 'l' as const,
-          target: '_self' as const,
+          size: 'l',
+          target: '_self',
           onClick: button.onClick,
         };
+        const ButtonComponent = button.style === 'outline' ? ButtonOutline : ButtonFilled;
 
-        return button.style === 'outline' ? <ButtonOutline {...buttonProps} /> : <ButtonFilled {...buttonProps} />;
+        return <ButtonComponent key={button.uniqueId} {...buttonProps} />;
       })}
     </div>
   );
