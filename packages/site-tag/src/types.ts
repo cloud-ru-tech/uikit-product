ae64aa270cd23803ed681e81105611d0e52e9495
daparfirov <daparfirov@cloud.ru>
2026-07-23 15:44:52 +0400

    feat(SITE-11714): update type site-tag

--- packages/site-tag/src/types.ts
+++ packages/site-tag/src/types.ts
@@ -1,3 +1,3 @@
 import { PromoTagProps } from '@snack-uikit/promo-tag';
 
-export type Appearance = NonNullable<Exclude<PromoTagProps['appearance'], 'primary' | 'yellow'>>;
+export type Appearance = NonNullable<Exclude<PromoTagProps['appearance'], 'primary'>>;
