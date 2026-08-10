f974026f2082a4fadb3b1bb243972ef0264d3bc5
Alexey Grudnov <aagrudnov@cloud.ru>
2026-08-10 13:55:55 +0300

    feat(CP-20360): add icon name type

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -1,12 +1,7 @@
 export { Sprite } from '@snack-uikit/icons';
 
-export * from './components/flag-icons';
-export * from './components/services-icons-old';
-export * from './components/extension-icons';
-export * from './components/logo-icons';
-export * from './components/interface-icons-old';
-export * from './components/interface-icons-product';
-export * from './components/interface-icons-system';
-export * from './components/service-icons';
+export * from './components';
 
 export { default as SpriteSystemSVG } from './sprite/svg/sprite-system.symbol.svg';
+
+export type { IconName } from './types';
---
+++ packages/icons/src/types.ts
@@ -0,0 +1,1 @@
+export type IconName = Extract<keyof typeof import('./components'), string>;
