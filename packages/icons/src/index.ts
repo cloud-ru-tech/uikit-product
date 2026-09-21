6fe3cef9ead219e36afda9680c3feecba991b140
Sergei Khlupin <svhlupin@cloud.ru>
2026-09-21 14:18:41 +0300

    fix(FF-9159): sprite building

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -1,7 +1,7 @@
 export { Sprite } from '@snack-uikit/icons';
 
+export type { IconName } from './types';
+
 export * from './components';
 
 export { default as SpriteSystemSVG } from './sprite/svg/sprite-system.symbol.svg';
-
-export type { IconName } from './types';
